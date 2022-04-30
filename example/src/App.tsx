43b65b7da4211469918b5a38e1acfa83870e052b
Robert Soriano <sorianorobertc@gmail.com>
2022-04-29 22:13:15 -0700

    add header buttons in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,21 +1,48 @@
 import * as React from 'react';
-
-import { StyleSheet, View } from 'react-native';
-import { SketchCanvas } from 'rn-perfect-sketch-canvas';
+import { useRef } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  useWindowDimensions,
+} from 'react-native';
+import { SketchCanvas, SketchCanvasRef } from 'rn-perfect-sketch-canvas';
+import Header from './components/Header';
 
 export default function App() {
+  const { width } = useWindowDimensions();
+  const canvasRef = useRef<SketchCanvasRef>(null);
+
   return (
-    <View style={styles.container}>
-      <SketchCanvas />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: '#f0f0f0',
+          flex: 1,
+          alignItems: 'center',
+        }}
+      >
+        <Header canvasRef={canvasRef} />
+        <View
+          style={{
+            width: width - 24,
+            flexGrow: 1,
+            backgroundColor: '#ffffff',
+            borderRadius: 10,
+            overflow: 'hidden',
+            elevation: 1,
+          }}
+        >
+          <SketchCanvas ref={canvasRef} containerStyle={styles.container} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
     width: 60,
---
+++ example/src/components/Header.tsx
@@ -0,0 +1,99 @@
+import React, { MutableRefObject } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import type { SketchCanvasRef } from 'rn-perfect-sketch-canvas';
+
+interface Props {
+  canvasRef: MutableRefObject<SketchCanvasRef | null>;
+}
+
+const Header: React.FC<Props> = ({ canvasRef }) => {
+  /**
+   * Reset the canvas & draw state
+   */
+  const reset = () => {};
+
+  const save = () => {
+    // save as svg
+  };
+
+  const undo = () => {
+    canvasRef.current?.undo();
+  };
+
+  const redo = () => {
+    canvasRef.current?.redo();
+  };
+
+  return (
+    <View
+      style={{
+        height: 50,
+        width: '100%',
+        paddingHorizontal: 12,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <View
+        style={{
+          flexDirection: 'row',
+        }}
+      >
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={undo}
+          style={[styles.button, { marginRight: 10 }]}
+        >
+          <Text style={styles.buttonText}>Undo</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={redo}
+          activeOpacity={0.6}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Redo</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'row',
+        }}
+      >
+        <TouchableOpacity
+          onPress={reset}
+          activeOpacity={0.6}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Reset</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={save}
+          style={[styles.button, { marginLeft: 10 }]}
+        >
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: 12,
+    backgroundColor: 'white',
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+  },
+  buttonText: {
+    color: 'black',
+  },
+});
+
+export default Header;
